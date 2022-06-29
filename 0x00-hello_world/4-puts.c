8d458977c79c6f62d18da157c96e07db57d81bb9
Dr-MarcusI <oshomarcus20@gmail.com>
2022-06-29 13:03:08 -0700

    perferct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+
 int main(void)
-
-
-
 {
+
+/* Write a C program that prints exactly */
 
 puts("\"Programming is like building a multilingual puzzle\n");
 
 
-
 return (0);
-
-
 
 }
 
