9a6cecdf7a198c9545da589a0309f2a2f59d2f2e
Dr-MarcusI <oshomarcus20@gmail.com>
2022-06-29 15:04:40 -0700

    move

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ int main(void)
 {
 
 
-printf("Size of a char: 1 byte(s)\n", sizeof(char));
-printf("Size of a int: 4 byte(s)\n", sizeof(int));
-printf("Size of a long int: 4 byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: 8 byte(s)\n", sizeof(float));
+rintf("Size of a char: 1 byte(s)");
+printf("Size of an int: 4 byte(s))";
+printf(Size of a long int: 4 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
 
 
 return (0);
