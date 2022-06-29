7bed98506ffba42cea529b4d98c50f317ea965ca
Dr-MarcusI <oshomarcus20@gmail.com>
2022-06-29 15:11:29 -0700

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 {
 
 
-rintf("Size of a char: 1 byte(s)");
+printf("Size of a char: 1 byte(s)");
 printf("Size of an int: 4 byte(s))";
 printf(Size of a long int: 4 byte(s)");
 printf("Size of a long long int: 8 byte(s)");
