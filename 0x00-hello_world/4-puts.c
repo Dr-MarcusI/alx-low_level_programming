7aae02e99996b17eb63d044c030abcd969ea6e04
Dr-MarcusI <oshomarcus20@gmail.com>
2022-06-29 12:53:27 -0700

    corre

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
-
 int main(void)
-{
-	
-puts("Programming is like building a multilingual puzzle\n");
 
-	
-	
+
+
+{
+
+puts("\"Programming is like building a multilingual puzzle\n");
+
+
+
 return (0);
+
+
+
 }
+
+
+
