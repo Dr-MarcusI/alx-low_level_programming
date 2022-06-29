87371f3de25232c4f83a4a67533b04cbd10c75f1
Dr-MarcusI <oshomarcus20@gmail.com>
2022-06-29 12:23:08 -0700

    braces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
+
 int main(void)
-
 {
+	
+puts("Programming is like building a multilingual puzzle\n");
 
-
-puts("\"Programming is like building a multilingual puzzle");
+	
+	
 return (0);
-
-
 }
-
