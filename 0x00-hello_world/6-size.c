cdcaec78145dcb531eb4b67038e91b989fc2d5ba
Dr-MarcusI <oshomarcus20@gmail.com>
2022-06-29 17:02:44 -0700

    as

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,23 @@
  *Description:'Write a C program that prints exact
  *Return:Always 0 (Success)
  */
-int main(void)
+
+
+int main()
+
+
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s))\n", sizeof(int));
 printf(Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+
 return (0);
+
+
+
+
+
 }
