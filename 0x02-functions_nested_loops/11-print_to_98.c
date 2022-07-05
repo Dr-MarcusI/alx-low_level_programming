52fbce7b7c34a883781c39ba5ce17363fcb79eae
Dr-MarcusI <oshomarcus20@gmail.com>
2022-07-04 23:30:35 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+/*
+*File: 11-print_to_98.c
+*Auth: Dr Marcus Imagwe
+*/
+#include <stdio.h>
+/**
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
+ */
+
+void print_to_98(int n)
+{
+
+while (n < 98)
+{
+	printf("%i, ", n);
+	n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+
+printf("98");
+putchar('\n');
+}
