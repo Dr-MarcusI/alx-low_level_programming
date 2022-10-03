a1955be490ceb4912fc14aee9704daf1b3d3dd25
Dr-MarcusI <oshomarcus20@gmail.com>
2022-10-03 06:12:53 -0700

    first correction

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp/ https://raw.github.com/david-develop/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libran.so
+export LD_PRELOAD=/tmp/libran.so
