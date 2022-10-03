1e30d462dc8e9fd47467e2b76166aaa994f54908
Dr-MarcusI <oshomarcus20@gmail.com>
2022-10-03 06:24:41 -0700

    thrid correction

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,10 @@
+#ifndef _100_OPERATIONS_H_
+#define _100_OPERATIONS_H_
+
+int add(int num1, int num2);
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int div(int num1, int num2);
+int mod(int num1, int num2);
+
+#endif /*_100_OPERATIONS_H_*/
