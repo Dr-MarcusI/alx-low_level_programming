3a98771c25c497b1d8f36921dbc67e19360083e3
Dr-MarcusI <oshomarcus20@gmail.com>
2022-10-03 20:56:35 -0700

    test2

---
+++ 0x18-dynamic_libraries/TEST2/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /*MAIN_H*/
